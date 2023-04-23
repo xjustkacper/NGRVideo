d66bf6e7d145980736daa00659965b561411db8f
MarcoDab <131591144+MarcoDab@users.noreply.github.com>
2023-04-23 15:30:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,21 @@
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-</head>
+    <style> /*DODAC POTEM DO CSS */
+.top{
+  text-align: center;
+}
+.recent
+{
+  text-align: center;
+}
+.ad {
+  text-align: center;
+  
+  bottom: 0px;
+}
+</style>
+  </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
@@ -45,6 +59,62 @@
     </div>
   </div>
 </nav>
+<div class="top" >
+  
+<a>TOP 10 <br /></a>
+  <button id="p2" style="display:none;"> < </button>
+  <img src="https://placehold.jp/216x320.png" alt="movie"> <!--Rozmiar plakatu 2592x3840 :12 216x320 w pikselach-->
+  <img src="https://placehold.jp/216x320.png" alt="movie">
+  <img src="https://placehold.jp/216x320.png" alt="movie">
+  <img src="https://placehold.jp/216x320.png" alt="movie">
+  <img src="https://placehold.jp/216x320.png" alt="movie">
+  <button id="p1" style="display:;"> > </button>
+          </div>
+          <div class="recent" >
+<a>NAJNOWSZE <br /></a>
+<button id="p4" style="display:none;"> < </button>
+  <img src="https://placehold.jp/216x320.png" alt="movie"> 
+  <img src="https://placehold.jp/216x320.png" alt="movie">
+  <img src="https://placehold.jp/216x320.png" alt="movie">
+  <img src="https://placehold.jp/216x320.png" alt="movie">
+  <img src="https://placehold.jp/216x320.png" alt="movie">
+  <button id="p3" style="display:;"> > </button>
+        </div>
 
+<script> // skrypt w js do przyciskow
+ var a = 0;
+  var p1 = document.getElementById("p1");
+  var p2 = document.getElementById("p2");
+  var p3 = document.getElementById("p3");
+  var p4 = document.getElementById("p4");
+
+p1.addEventListener("click", function() {
+  p1.style.display = "none";
+  p2.style.display = "";
+});
+
+p2.addEventListener("click", function() {
+  p2.style.display = "none";
+  p1.style.display = "";
+});
+p3.addEventListener("click", function() {
+  p4.style.display = "";
+  a++;
+});
+
+p4.addEventListener("click", function() {
+  a--;
+  if (a===0){
+  p4.style.display = "none";
+  }
+  
+});
+</script>
+<div class="ad">
+  <br />
+    <img src="https://placehold.jp/800x180.png" alt="Reklama">
+    
+  
+</div>
 </body>
 </html>
