c0ffde73c12b434c9920112ce443f50de811cfb2
MarcoDab <131591144+MarcoDab@users.noreply.github.com>
2023-05-20 17:42:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,37 +8,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NGRVideo</title>
-    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <style> /*DODAC POTEM DO CSS */
-.top{
-  text-align: center;
-}
-.recent
-{
-  text-align: center;
-}
-.ad {
-  text-align: center;
-  
-  bottom: 0px;
-}
-</style>
-
+    <!-- Do karuzeli  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+        integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+        crossorigin="anonymous" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+        integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
+        crossorigin="anonymous" />
   </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
     <div class="d-flex align-items-center">
-      <a class="navbar-brand" href="index.php">NGRVideo</a>
+      <a class="navbar-brand" href="#">NGRVideo</a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="datamovies.php">Baza filmów</a>
+          <a class="nav-link" href="#">Baza filmów</a>
         </li>
       </ul>
-      <form class="d-flex mx-3" action="search.php" method="POST" role="search">
-        <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search">
+      <form class="d-flex mx-3" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
@@ -46,80 +39,234 @@
       <ul class="navbar-nav ms-auto">
 					<?php
 						if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-              echo '<li class="navbar-text">Witaj, niezalogowany</li>';
+                            echo '<li class="navbar-text">Witaj, niezalogowany</li>';
 							echo '<li class="nav-item"><a class="nav-link" href="login.php">Logowanie</a></li>';
 							echo '<li class="nav-item"><a class="nav-link" href="register.php">Rejestracja</a></li>';
 						} 
-            else {
-              echo '<li class="navbar-text">Witaj, '.$_SESSION["login"].'</li>';
+                        else {
+                            echo '<li class="navbar-text">Witaj, '.$_SESSION["login"].'</li>';
 							echo '<li class="nav-item"><a class="nav-link" href="profile.php">Profil</a></li>';
-              echo '<li class="nav-item"><a class="nav-link" href="logout.php">Wyloguj się</a></li>';
-						
-						}
+                            echo '<li class="nav-item"><a class="nav-link" href="logout.php">Wyloguj się</a></li>';
+						      }
 					?>
 				</ul>
-
     </div>
   </div>
 </nav>
-<div class="top" >
-  
-<a>TOP 10 <br /></a>
-  <button id="p2" style="display:none;"> < </button>
-  <img src="https://placehold.jp/216x320.png" alt="movie"> <!--Rozmiar plakatu 2592x3840 :12 216x320 w pikselach-->
-  <img src="https://placehold.jp/216x320.png" alt="movie">
-  <img src="https://placehold.jp/216x320.png" alt="movie">
-  <img src="https://placehold.jp/216x320.png" alt="movie">
-  <img src="https://placehold.jp/216x320.png" alt="movie">
-  <button id="p1" style="display:;"> > </button>
-          </div>
-          <div class="recent" >
-<a>NAJNOWSZE <br /></a>
-<button id="p4" style="display:none;"> < </button>
-  <img src="https://placehold.jp/216x320.png" alt="movie"> 
-  <img src="https://placehold.jp/216x320.png" alt="movie">
-  <img src="https://placehold.jp/216x320.png" alt="movie">
-  <img src="https://placehold.jp/216x320.png" alt="movie">
-  <img src="https://placehold.jp/216x320.png" alt="movie">
-  <button id="p3" style="display:;"> > </button>
+
+  <!-- <img src="https://placehold.jp/216x320.png" alt="movie"> -->
+
+
+
+<section>
+  <div class="container-fluid my-5" >
+    <h1 class="text-center fw-bold ">TOP 10</h1>
+    <div class="row">
+    <div class="owl-carousel owl-theme">
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title</h4>
         </div>
-        <script> // skrypt w js do przyciskow
- var a = 0;
-  var p1 = document.getElementById("p1");
-  var p2 = document.getElementById("p2");
-  var p3 = document.getElementById("p3");
-  var p4 = document.getElementById("p4");
-
-p1.addEventListener("click", function() {
-  p1.style.display = "none";
-  p2.style.display = "";
-});
-
-p2.addEventListener("click", function() {
-  p2.style.display = "none";
-  p1.style.display = "";
-});
-p3.addEventListener("click", function() {
-  p4.style.display = "";
-  a++;
-});
-
-p4.addEventListener("click", function() {
-  a--;
-  if (a===0){
-  p4.style.display = "none";
-  }
-  
-});
-</script>
-<div class="ad">
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title2</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title3</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title4</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title5</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title6</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title7</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title8</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title9</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title 10</h4>
+        </div>
+      </div>
+    </div>
+    </div>
+</div>
+</div>
+</section>
+<section>
+  <div class="container-fluid my-5" >
+    <h1 class="text-center fw-bold ">Nowe</h1>
+    <div class="row">
+    <div class="owl-carousel owl-theme">
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title2</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title3</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title4</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title5</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title6</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title7</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title8</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title9</h4>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="card">
+        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+        <div class="card-body">
+         <h4>Movie title 10</h4>
+        </div>
+      </div>
+    </div>
+    </div>
+</div>
+</div>
+</section>
+<div>
   <br />
-    <img src="https://placehold.jp/800x180.png" alt="Reklama">
-    
-  
+    <img src="https://placehold.jp/800x180.png" alt="Reklama"> 
 </div>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
-
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+    integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+    crossorigin="anonymous"></script>
+<script>
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 15,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+</script>
 </body>
 </html>
