9f8d44cafa288684cd0a697634cb519099448cb3
xjustkacper <51298603+xjustkacper@users.noreply.github.com>
2023-05-27 17:04:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
-  session_start();
+session_start();
+require_once "connect.php";
+
+$conn = new mysqli($host, $db_user, $db_pass, $db_name);
+
+$sql1 = "SELECT tytul FROM filmy";
+$sql2 = "SELECT tytul FROM filmy";
+
+$stmt = $conn->prepare($sql1);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$tytul = [];
+while ($row = $result->fetch_assoc()) {
+    $tytul[] = $row;
+}
+
+$stmt->close();
+$conn->close();
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -21,17 +39,17 @@
         crossorigin="anonymous" />
   </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
     <div class="d-flex align-items-center">
-      <a class="navbar-brand" href="#">NGRVideo</a>
+      <a class="navbar-brand" href="index.php">NGRVideo</a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#">Baza filmów</a>
+          <a class="nav-link" href="datamovies.php">Baza filmów</a>
         </li>
       </ul>
-      <form class="d-flex mx-3" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <form class="d-flex mx-3" action="search.php" method="POST" role="search">
+        <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
@@ -39,17 +57,19 @@
       <ul class="navbar-nav ms-auto">
 					<?php
 						if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-                            echo '<li class="navbar-text">Witaj, niezalogowany</li>';
+              echo '<li class="navbar-text">Witaj, niezalogowany</li>';
 							echo '<li class="nav-item"><a class="nav-link" href="login.php">Logowanie</a></li>';
 							echo '<li class="nav-item"><a class="nav-link" href="register.php">Rejestracja</a></li>';
 						} 
-                        else {
-                            echo '<li class="navbar-text">Witaj, '.$_SESSION["login"].'</li>';
+            else {
+              echo '<li class="navbar-text">Witaj, '.$_SESSION["login"].'</li>';
 							echo '<li class="nav-item"><a class="nav-link" href="profile.php">Profil</a></li>';
-                            echo '<li class="nav-item"><a class="nav-link" href="logout.php">Wyloguj się</a></li>';
-						      }
+              echo '<li class="nav-item"><a class="nav-link" href="logout.php">Wyloguj się</a></li>';
+						
+						}
 					?>
 				</ul>
+
     </div>
   </div>
 </nav>
@@ -63,85 +83,20 @@
     <h1 class="text-center fw-bold ">TOP 10</h1>
     <div class="row">
     <div class="owl-carousel owl-theme">
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title2</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title3</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title4</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title5</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title6</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title7</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title8</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title9</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title 10</h4>
-        </div>
-      </div>
+    <?php 
+        if (count($tytul) > 0) {
+            foreach ($tytul as $tytuly): ?>
+                <div class="item">
+                  <div class="card">
+                    <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+                    <div class="card-body">
+                      <h4><?php echo $tytuly["tytul"]?></h4>
+                    </div>
+                  </div>
+                </div>
+            <?php endforeach;
+        } 
+        ?>
     </div>
     </div>
 </div>
@@ -151,90 +106,24 @@
   <div class="container-fluid my-5" >
     <h1 class="text-center fw-bold ">Nowe</h1>
     <div class="row">
-    <div class="owl-carousel owl-theme">
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title</h4>
-        </div>
+      <div class="owl-carousel owl-theme">
+        <?php 
+        if (count($tytul) > 0) {
+            foreach ($tytul as $tytuly): ?>
+                <div class="item">
+                  <div class="card">
+                    <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
+                    <div class="card-body">
+                      <h4><?php echo $tytuly["tytul"]?></h4>
+                    </div>
+                  </div>
+                </div>
+            <?php endforeach;
+        } 
+        ?>
       </div>
     </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title2</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title3</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title4</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title5</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title6</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title7</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title8</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title9</h4>
-        </div>
-      </div>
-    </div>
-    <div class="item">
-      <div class="card">
-        <img src="https://placehold.jp/216x320.png" alt="image" class="card-img-top">
-        <div class="card-body">
-         <h4>Movie title 10</h4>
-        </div>
-      </div>
-    </div>
-    </div>
-</div>
-</div>
+  </div>
 </section>
 <div>
   <br />
