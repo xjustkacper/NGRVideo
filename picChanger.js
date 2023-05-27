fec771f2651ff388f5624245099bb24eeaf42a5e
xjustkacper <51298603+xjustkacper@users.noreply.github.com>
2023-05-27 17:03:43 +0200

    Delete picChanger.js

--- picChanger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let button = document.querySelector("#buttonchange");
-
-    button.addEventListener("click", function() {
-        let picurl = document.querySelector("#input").value;
-        document.querySelector("#pfpic").src = picurl;
-    });
-});
