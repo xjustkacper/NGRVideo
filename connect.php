4827e20e2115195c9d5276297e9382c8dc029b60
Kacper <51298603+xjustkacper@users.noreply.github.com>
2023-06-11 13:15:26 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,15 @@
- <?php
+<?php
 
+// Adres IP serwera bazy danych, zazwyczaj dla lokalnego serwera to 127.0.0.1
 $host = "127.0.0.1";
+
+// Nazwa użytkownika bazy danych, dla lokalnego serwera zazwyczaj 'root'
 $db_user = "root";
+
+// Hasło do bazy danych, na lokalnym serwerze często jest puste
 $db_pass = "";
+
+// Nazwa bazy danych, do której chcemy się połączyć
 $db_name = "ngrvideo";
 
  ?>
